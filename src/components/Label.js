215477e0e8ce52908a023983ac03ad29984c0c81
Jorge Atempa <atempaitm@gmail.com>
2022-09-02 21:25:20 -0700

    Remove margin property

--- src/components/Label.js
+++ src/components/Label.js
@@ -2,7 +2,7 @@ import styled from "styled-components"
 
 const Label = styled.span`
   font-size: 1.2rem;
-  margin-right: 10px;
+  font-weight: bold;
 
   @media (max-width: 600px) {
     font-size: 1rem;
