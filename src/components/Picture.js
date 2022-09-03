48f223b7634e17119b95487e61e6eba492337e7c
Jorge Atempa <atempaitm@gmail.com>
2022-09-02 20:52:02 -0700

    Set high property

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -3,6 +3,18 @@ import styled from "styled-components"
 
 const ProfilePicture = styled.img`
   width: 100%;
+  height: 250px;
+  max-height: 250px;
+
+  @media (max-width: 800px) {
+    height: 200px;
+    max-height: 200px;
+  }
+
+  @media (max-width: 600px) {
+    height: 150px;
+    max-height: 150px;
+  }
 `
 
 const Picture = props => {
