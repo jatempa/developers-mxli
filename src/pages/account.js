94753522a59ee4c4b972725e34c68d8c85c6186c
Jorge Atempa <atempaitm@gmail.com>
2022-11-16 20:28:30 -0800

    Update account top bar styles

--- src/pages/account.js
+++ src/pages/account.js
@@ -4,21 +4,46 @@ import { Link } from "gatsby"
 import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react"
 
 import LogoutButton from "../components/LogoutButton"
+import styled from "styled-components"
+
+const NavigationContainer = styled.nav`
+  margin-top: 1em;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const NavigationItem = styled.div`
+  flex-basis: 150px;
+  text-align: center;
+`
+
+const NavigationProfileItem = styled(NavigationItem)`
+  flex-basis: 200px;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`
 
 const Account = () => {
   const { user } = useAuth0()
   return (
-    <>
-      <nav>
+    <NavigationContainer>
+      <NavigationItem>
         <Link to="/">Home</Link>
-        <p>
-          Nombre: {user.given_name} {user.family_name}
-        </p>
-        <p>Email: {user.email}</p>
-        <LogoutButton />
-      </nav>
-      <img src={user.picture} alt={`${user.nickname && user.email}_profile`} />
-    </>
+      </NavigationItem>
+      <NavigationProfileItem>
+        <Link to="/">
+          <img
+            src={user.picture}
+            alt={`${user.nickname && user.email}_profile`}
+          />
+        </Link>
+        <span>
+          <LogoutButton />
+        </span>
+      </NavigationProfileItem>
+    </NavigationContainer>
   )
 }
 
