7486f5597c267e737a171c256addd736b0a71859
Jorge Atempa <atempaitm@gmail.com>
2023-02-23 19:17:20 -0800

    Update api environment variable

--- src/hooks/useProfiles.js
+++ src/hooks/useProfiles.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-const API_URL = process.env.API_URL
+const API_URL = process.env.GATSBY_API_URL
 
 const useProfiles = () => {
   const [name, setName] = useState("")
